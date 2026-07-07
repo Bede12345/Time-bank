dea4db23179ae6c1939db772af7daaf2d7f46d08
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-07 00:06:00 -0700

    started create transaction func

--- time-bank/backend/src/controllers/transactionController.js
+++ time-bank/backend/src/controllers/transactionController.js
@@ -4,3 +4,14 @@ const User = require('../models/User');
 const { logger } = require('../utils/logger');
 const { validationResult } = require('express-validator');
 
+exports.createTransaction = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { offer_id, hours_estimated } = req.body;
+        const requesterId = req.userId;
+
+        

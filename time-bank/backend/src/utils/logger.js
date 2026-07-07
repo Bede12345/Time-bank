dc80260e8ac2578011a5a6d6990e57e3f0f4a5b8
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-06 22:00:53 -0700

    finished initializing before func

--- time-bank/backend/src/utils/logger.js
+++ time-bank/backend/src/utils/logger.js
@@ -7,4 +7,24 @@ const logger = winston.createLogger({
         winston.format.timestamp({
             format: 'YYYY-MM-DD HH:mm:ss'
         }),
-        
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({
+            filename: path.join(__dirname, '../../logs/error.log'),
+            level: 'error'
+        }),
+        new winston.transports.File({
+            filename: path.join(__dirname, '../../logs/combined.log')
+        }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        })
+    ]
+});
+

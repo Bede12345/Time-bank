f60f6db520e8dc93020873a820ce76d16bf17789
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-07 09:10:16 -0700

    created get user profile func

--- time-bank/backend/src/controllers/userController.js
+++ time-bank/backend/src/controllers/userController.js
@@ -2,3 +2,13 @@ const User = require('../models/User');
 const { logger } = require('../utils/logger');
 const { validationResult } = require('express-validator');
 
+exports.getUserProfile = async (req, res) => {
+    try {
+        const userId = req.params.id || req.userId;
+        const user = await User.findById(userId);
+        
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        

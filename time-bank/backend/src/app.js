6dc3ebaf416cdecd661813d184bd1f0bd5864936
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-07 00:42:12 -0700

    added route imports

--- time-bank/backend/src/app.js
+++ time-bank/backend/src/app.js
@@ -5,3 +5,9 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const { requestLogger } = require('./utils/logger');
 
+const authRoutes = require('./routes/authRoutes');
+const offerRoutes = require('./routes/offerRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
+
+const app = express();
+

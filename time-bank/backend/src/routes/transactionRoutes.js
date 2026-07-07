130d944305959fd145533cd3738204c0427d758d
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-07 00:38:25 -0700

    working on transactionroutes

--- time-bank/backend/src/routes/transactionRoutes.js
+++ time-bank/backend/src/routes/transactionRoutes.js
@@ -2,3 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const { authenticate } = require('../middleware/auth');
+const {
+    createTransaction,
+    getTransactions,
+    getTransaction,
+    confirmCompletion,
+    updateTransactionStatus
+} = require('../controllers/transactionController');
+

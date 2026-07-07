9b5eec835a5993e96f6ca2de68db776a37fca091
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-06 22:48:39 -0700

    created token generator func

--- time-bank/backend/src/controllers/authController.js
+++ time-bank/backend/src/controllers/authController.js
@@ -4,3 +4,11 @@ const bcrypt = require('bcrypt');
 const { logger } = require('../utils/logger');
 const { validationResult } = require('express-validator');
 
+const generateToken = (userId) => {
+    return jwt.sign(
+        { userId },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRE }
+    );
+};
+

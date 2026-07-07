47f33b89ed4429e1a30771a062a8fa9f7e246ae1
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-07 00:43:45 -0700

    added routes and middleware

--- time-bank/backend/src/app.js
+++ time-bank/backend/src/app.js
@@ -11,3 +11,14 @@ const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger);
+app.use(morgan('dev'));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/offers', offerRoutes);
+app.use('/api/transactions', transactionRoutes);
+

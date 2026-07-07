03a288fb4f94c86217ac281d33c37f84f948446e
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-07 10:11:15 -0700

    adding some custom error classes

--- time-bank/backend/src/utils/errors.js
+++ time-bank/backend/src/utils/errors.js
@@ -7,3 +7,18 @@ class AppError extends Error {
     }
 }
 
+class ValidationError extends AppError {
+    constructor(message, errors = []) {
+        super(message, 400);
+        this.errors = errors;
+        this.name = 'ValidationError';
+    }
+}
+
+class AuthenticationError extends AppError {
+    constructor(message = 'Authentication required') {
+        super(message, 401);
+        this.name = 'AuthenticationError';
+    }
+}
+

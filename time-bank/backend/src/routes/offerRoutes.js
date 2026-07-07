274ad6670ba91ffc9b403147f5a7d79c9d8e977b
Bede12345 <Bedemariyamtamrat@gmail.com>
2026-07-07 00:34:20 -0700

    working on offerroutes

--- time-bank/backend/src/routes/offerRoutes.js
+++ time-bank/backend/src/routes/offerRoutes.js
@@ -2,3 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const { authenticate } = require('../middleware/auth');
+const {
+    createOffer,
+    getOffers,
+    getOffer,
+    getMyOffers,
+    updateOfferStatus,
+    deleteOffer
+} = require('../controllers/offerController');
+
